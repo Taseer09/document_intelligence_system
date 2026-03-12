af93ca77b1a1fb4d2017c77916d51adca20b9ac4
Taseer09 <ua7385424@gmail.com>
2026-03-12 14:18:04 +0500

    Multi-Document Knowledge Base Added to Loader.py

--- app/loader.py
+++ app/loader.py
@@ -1,10 +1,22 @@
+import os
 from langchain_community.document_loaders import PyPDFLoader
 
-path = "D:\AI\AI Data\document_intelligence_system\data\sample.pdf"
 
-def load_pdf(path):
+def load_documents(folder_path):
 
-    loader = PyPDFLoader(path)
-    documents = loader.load()
+    documents = []
+
+    for file in os.listdir(folder_path):
+
+        if file.endswith(".pdf"):
+
+            loader = PyPDFLoader(os.path.join(folder_path, file))
+
+            docs = loader.load()
+
+            for doc in docs:
+                doc.metadata["source"] = file
+
+            documents.extend(docs)
 
     return documents
